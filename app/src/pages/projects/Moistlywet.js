888c6b18c5d626a625fc7af7f274a3307cae5daa
John Oram <john@oram.ca>
2019-07-14 13:27:48 -0700

    first pass describing side projects and self

--- app/src/pages/projects/Moistlywet.js
+++ app/src/pages/projects/Moistlywet.js
@@ -1,10 +1,26 @@
 import React from "react";
 import BasePage from "../BasePage";
+import icon from "../../static/moistlywet.png";
 
 
 function Moistlywet() {
-  return (<BasePage title="Moistlywet">
-      <a href="https://app.moistlywet.com">app.moistlywet.com</a>
+    let title = <>
+      <img src={icon} />
+      Distillery
+    </>;
+
+    return (<BasePage title={title}>
+        This project is to auto-water plants. We started it because a friend/coworker had never soldered before, and plants/gardening was another shared interest.
+        During a lunch hour a week, we'd have "soldeirng club", where those of us with side projects, circuitry related or not, would meet, discuss progress and blockers, and, of course, solder.
+        The setup is relatively simple, with three components: a web-interface, an api, and a small micro-controller with a builtin wifi chip.
+        The micro-controller periodically reads the moisture of the soil, and reports the results to the API. In the response, the API decides how much watering, if any must be done, and how long until the micro-controller should poll again.
+        The web interface shows the datapoints on a graph, and allows the user to adjust the min and max moisture levels.
+        <br/>
+        <br/>
+
+        You can see the code here: <a href="https://github.com/joram/moistlywet">https://github.com/joram/moistlywet</a>
+        <br/>
+        and the website here: <a href="https://app.moistlywet.com">app.moistlywet.com</a>
     </BasePage>
   )
 
--- app/src/pages/projects/Triptracks.js
+++ app/src/pages/projects/Triptracks.js
@@ -1,9 +1,26 @@
 import React from "react";
 import BasePage from "../BasePage";
+import icon from "../../static/triptracks.png";
 
 function Triptracks() {
-  return (<BasePage title="Triptracks">
-      <a href="https://app.triptracks.com">app.triptracks.com</a>
+    let title = <>
+      <img src={icon} />
+      Triptracks
+    </>;
+
+    return (<BasePage title={title}>
+        This is a project to help with mountaineering trips. Specifically concentrating on gps tracks.
+        Currently the functionality is limited to browsing routes via a web interface, or a mobile app.
+        This has been a testbed project for different geo databases, and graphql over lambda functions (works surprisingly well).
+        <br/><br/>
+
+        The future features list is extensive, and includes offline caching of routes and map tiles for areas you are interested (there is likely no internet access on the trip).
+        Trip planning, including: shared packing lists, inviting partners, forecast alerts, and generated trip plan documents to leave with someone in case search and rescue needs to be called.
+        <br/><br/>
+
+        You can see the code here: <a href="https://github.com/joram/triptracks">https://github.com/joram/triptracks</a>
+        <br/>
+        and the website here: <a href="https://app.triptracks.io">app.triptracks.io</a>
     </BasePage>)
 }
 
