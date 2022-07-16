d693faebf2ecc96f22203d85da47b3f9b7f46fe0
John Oram <john@oram.ca>
2022-07-16 11:27:33 -0700

    clean up technologies used page

--- app/src/pages/work/BaseCompanyPage.jsx
+++ app/src/pages/work/BaseCompanyPage.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import {Card, Container, Divider} from "semantic-ui-react"
+import {Card, Divider} from "semantic-ui-react"
 import BasePage from "../BasePage";
-import ReactWordcloud from "react-wordcloud";
 
+function splitList(items, chunkSize){
+    let chunks = [];
+    for (let i = 0; i < items.length; i += chunkSize) {
+        const chunk = items.slice(i, i + chunkSize)
+        chunks.push(chunk)
+    }
+    return chunks
+}
+
+function Technologies(props){
+    let {technologiesUsed} = props
+    technologiesUsed = technologiesUsed.filter((v, i, a) => a.indexOf(v) === i)
+
+    let i=0;
+    let rows = []
+    splitList(technologiesUsed, 5).forEach(techUsed => {
+        rows.push(<div key={"tech_row_"+i}>
+            <center>{techUsed.join(", ")}</center>
+        </div>)
+        i += 1
+    })
+    return <>
+        <Divider horizontal>Technologies Used</Divider>
+        {rows}
+    </>
+}
 function BaseCompanyPage(props) {
     let {companyName, description, roles} = props
     let roleCards = []
-    let technologiesUsed = {}
+    let technologiesUsed = []
     roles.forEach(role => {
         let {company, jobTitle, description, startDate, endDate, technologies} = role
         roleCards.push(<Card
@@ -16,45 +41,18 @@ function BaseCompanyPage(props) {
             extra={startDate+" - "+endDate}
         />)
         if(technologies !== undefined){
-            technologies.forEach(name => {
-                if(technologiesUsed[name] === undefined) {
-                    technologiesUsed[name] = 0
-                }
-                technologiesUsed[name] += 1
-            })
+            technologiesUsed = technologiesUsed.concat(technologies)
         }
     })
-    console.log(technologiesUsed)
-    let words = [];
-    Object.keys(technologiesUsed).forEach(name => {
-        let weight = technologiesUsed[name]
-        words.push({
-            text: name,
-            value: weight,
-          })
-    })
 
-    let options = {
-      rotations: 1,
-      rotationAngles: [0],
-    };
+
   return (<BasePage title={companyName}>
       <center>{description}</center>
       <Divider horizontal>Roles</Divider>
       <Card.Group centered={true}>
           {roleCards}
       </Card.Group>
-      <Divider horizontal>Technologies Used</Divider>
-      <Container>
-          <div style={{
-            width: "fit-content",
-            margin: "auto",
-              height:"200px"
-          }}>
-          <ReactWordcloud rotationAngles={[0,0]} options={options} words={words} size={[500,200]} />
-
-          </div>
-      </Container>
+      <Technologies technologiesUsed={technologiesUsed}/>
 
     </BasePage>)
 }
