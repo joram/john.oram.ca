6f97229f2e3eeda382bd3dce7443baea7d955ad1
John Oram <john@oram.ca>
2025-08-30 07:30:54 -0700

    dark green

--- app/src/sidebar.tsx
+++ app/src/sidebar.tsx
@@ -34,7 +34,7 @@ function Sidebar({isMobile}: {isMobile: boolean}) {
         flex:"1",
     }
 
-    return <span id="nav" style={{backgroundColor:"purple"}}>
+    return <span id="nav" style={{backgroundColor:"darkgreen"}}>
         <Link to="/">
             <h3 style={{textAlign:"center"}}>
                 <div>
